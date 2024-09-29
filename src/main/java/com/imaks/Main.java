a3b6081d4d6da93bc2c46baecd332e2e5cd5328d
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-09-29 18:10:46 +0200

    Init commit

---
+++ src/main/java/com/imaks/Main.java
@@ -0,0 +1,12 @@
+package com.imaks;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Temperature Converter");
+        System.out.println("V0.1");
+
+    }
+
+
+}
