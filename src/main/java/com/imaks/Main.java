994ffefc8d476c66cf13cb4dd9de88cb12b20b30
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-09-29 18:45:05 +0200

    Add convesion from °F to °C

--- src/main/java/com/imaks/Main.java
+++ src/main/java/com/imaks/Main.java
@@ -1,12 +1,29 @@
 package com.imaks;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Temperature Converter");
         System.out.println("V0.1");
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("Enter fahrenheit's number to convert to celsius:");
+            double fahrenheit = scanner.nextDouble();
+            double celsius = fahrenheitToCelsius(fahrenheit);
+            System.out.println(fahrenheit + " °F = " + celsius + " °C");
+
+        }
+
     }
 
+    //The conversion formula from Fahrenheits to Celsius is (°F) - 32) * 5/9.
+public static double fahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 5/9;
 
+}
 }
